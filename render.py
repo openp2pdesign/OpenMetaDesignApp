6ac9d7044c026252f577ef821b44303c5f47ff2b
Massimo Menichinelli <info@openp2pdesign.org>
2013-04-03 19:42:30 +0200

    start to define render.py

--- render.py
+++ render.py
@@ -9,35 +9,73 @@
 # License: GPL v.3
 #
 
+
+
 import cairo
 import math
 from classes import *
 
+# Load data
 temp = project()
 temp.load("test2.meta")
 
-for j,i in enumerate(temp.steps):
-    print "---"
+# Calculate total actors
+totalActors = []
+for j in range(len(temp.steps)):
+    for k in temp.steps[j].actors:
+        if k not in totalActors:
+            totalActors.append(k)
+
+whiteBorder = 10
+canvasX = (whiteBorder*2)+len(temp.steps)*400
+canvasY = 50+len(totalActors)*200
+
+surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, canvasX, canvasY+whiteBorder)
+ctx = cairo.Context (surface)
+
+# Paint background
+ctx.set_source_rgb(1, 1, 1) # blue
+ctx.rectangle(0, 0, canvasX, canvasY+whiteBorder)
+ctx.fill()
+
+# Write the canvas title
+ctx.set_source_rgb(0, 0, 0)
+ctx.select_font_face("TitilliumText25L", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+ctx.set_font_size(32)
+ctx.move_to(10, 32)
+ctx.show_text("System Map")
+
+        
+
+
+areaCoord = {}
+
+# Draw the borders of the areas for the steps
+for j in range(len(temp.steps)):    
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.set_line_width(2)
+    ctx.set_dash([1.0])
+    ctx.move_to(10+j*400, 50)
+    ctx.line_to(10+j*400, canvasY) 
+    ctx.stroke()
+# Draw last border
+ctx.set_source_rgb(0, 0, 0)
+ctx.set_line_width(2)
+ctx.set_dash([1.0])
+final = len(temp.steps)
+ctx.move_to(10+final*400, 50)
+ctx.line_to(10+final*400, 50+len(totalActors)*200) 
+ctx.stroke()
+
+
+for j in range(len(temp.steps)):
+    print "---"        
     print temp.steps[j].title
     for l,k in enumerate(temp.steps[j].flows):
         print temp.steps[j].flows[l].actor1
         print temp.steps[j].flows[l].actor2
         print temp.steps[j].flows[l].direction
 
-surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, 1400, 1200)
-ctx = cairo.Context (surface)
-
-# paint background
-ctx.set_source_rgb(1, 1, 1) # blue
-ctx.rectangle(0, 0, 1400, 1200)
-ctx.fill()
-
-# Write a text
-ctx.set_source_rgb(0, 0, 0)
-ctx.select_font_face("TitilliumText25L", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-ctx.set_font_size(32)
-ctx.move_to(10, 32)
-ctx.show_text("System Map")
 
 stepsize = 200
 space = 40
@@ -46,7 +84,6 @@ barsOriginX = 20
 barsOriginY = 70
 flowsOriginX = 40
 flowsOriginY = barsOriginY + barsize + space
-
 
 flowsCoord = {1:{"x":1,"y":1, "label":"flow"}}
 coord = {}
