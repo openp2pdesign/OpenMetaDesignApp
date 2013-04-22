bafca02a6adf2b74d5991829b71b0761c693821b
Massimo Menichinelli <info@openp2pdesign.org>
2013-04-22 20:48:26 +0200

    fix semicolon mistake

--- modules/mdwriter.py
+++ modules/mdwriter.py
@@ -23,7 +23,7 @@ temp.load("test2.meta")
 fo = open("README.md", "w")
 
 # Write a title
-fo.write(temp.title+"\n");
+fo.write(temp.title+"\n")
 titleFormat = ""
 for i in temp.title:
     titleFormat += "="
@@ -32,8 +32,8 @@ fo.write(titleFormat)
 fo.write("\n")
 
 
-fo.write("# General Information\n");
-fo.write("\n");
+fo.write("# General Information\n")
+fo.write("\n")
 
 # Write founders
 founders = "The **" + temp.title + "** project has been started by "
@@ -45,27 +45,29 @@ founders += "."
 fo.write(founders+"\n");
 fo.write("Its metadesign component, that here explains how the project works and how it has been developed is now at version "+temp.version+"\n")
 fo.write("\n")
-
+fo.write("The project has been developed together and for a community that:\n")
+fo.write("")
+fo.write("\n")
 
 
 # Write img tag for process participation
-fo.write("# Participation Process\n");
-fo.write("\n");
-fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"participation_process.png"+'">'+"\n");
+fo.write("# Participation Process\n")
+fo.write("\n")
+fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"participation_process.png"+'">'+"\n")
 
 
 # Write img tag for actors and flows
-fo.write("# Actors and Flows in the process\n");
-fo.write("\n");
-fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"actors_flows_system.png"+'">'+"\n");
+fo.write("# Actors and Flows in the process\n")
+fo.write("\n")
+fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"actors_flows_system.png"+'">'+"\n")
 
 
 # Write img tag for business models, or text?
 
 # Write img tag for network analysis
-fo.write("# The network of interactions in the project\n");
-fo.write("\n");
-fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"network_interactions.png"+'">'+"\n");
+fo.write("# The network of interactions in the project\n")
+fo.write("\n")
+fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"network_interactions.png"+'">'+"\n")
 
 # Write license for metadesign project
 
