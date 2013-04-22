113a049690b241598e9bcf9a21e9a9a24d812645
Massimo Menichinelli <info@openp2pdesign.org>
2013-04-22 20:37:43 +0200

    develop mdwriter.py

--- modules/mdwriter.py
+++ modules/mdwriter.py
@@ -10,37 +10,61 @@
 # License: GPL v.3
 #
 
-from modules.classes import *
+#from modules.classes import *
 
+from classes import *
 
-a = project()
-a.load("test2.meta")
-print "Flows0",a.steps[0].flows
+# project, currentFolder
+
+temp = project()
+temp.load("test2.meta")
 
 # Open a file
 fo = open("README.md", "w")
 
 # Write a title
-fo.write(a.title+"\n");
+fo.write(temp.title+"\n");
 titleFormat = ""
-for i in a.title:
+for i in temp.title:
     titleFormat += "="
 titleFormat += "\n"
 fo.write(titleFormat)
 fo.write("\n")
 
 
-# Write a description
+fo.write("# General Information\n");
+fo.write("\n");
 
 # Write founders
+founders = "The **" + temp.title + "** project has been started by "
+for j,i in enumerate(temp.founders):
+    founders += i
+    if j > 0:
+        founders += ", "
+founders += "."
+fo.write(founders+"\n");
+fo.write("Its metadesign component, that here explains how the project works and how it has been developed is now at version "+temp.version+"\n")
+fo.write("\n")
 
 # Write img tag for process participation
+fo.write("# Participation Process\n");
+fo.write("\n");
+fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"participation_process.png"+'">'+"\n");
+
 
 # Write img tag for actors and flows
+fo.write("# Actors and Flows in the process\n");
+fo.write("\n");
+fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"actors_flows_system.png"+'">'+"\n");
+
 
 # Write img tag for business models, or text?
 
 # Write img tag for network analysis
+fo.write("# Actors and Flows in the process\n");
+fo.write("\n");
+fo.write('<img src="'+temp.repo+"/blob/master/metadesign/"+"network_interactions.png"+'">'+"\n");
+
 
 
 # Close opend file
